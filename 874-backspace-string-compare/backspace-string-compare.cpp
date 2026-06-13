d6976b197980e3026dc24b7f84efb1a901496676
Anii-hub <chauhananirudh866@gmail.com>
2026-06-14 00:14:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (64.06%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        string s1="";
+        string s2="";
+        for(int i=0;i<n;i++){
+            if(s[i]=='#'){
+                if(!s1.empty()){
+                    s1.pop_back();
+                }
+            }
+            else{
+            s1.push_back(s[i]);}
+        }
+        for(int i=0;i<m;i++){
+            if(t[i]=='#'){
+                if(!s2.empty()){
+                    s2.pop_back();
+                }
+            }
+            else{s2.push_back(t[i]);}
+        }
+        if(s1==s2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};

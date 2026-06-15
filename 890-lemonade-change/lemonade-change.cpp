1fe5a8f43a1c47d98fa062a85166ae786aebb385
Anii-hub <chauhananirudh866@gmail.com>
2026-06-15 10:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (74.85%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -6,27 +6,25 @@ public:
         int ten=0;
         for(int i=0;i<n;i++){
             if(bills[i]==5){
+                
                 five++;
             }
             else if(bills[i]==10){
                 if(five>0){
-                    five--;
-                    ten++;
-
-                }
+               
+                five--;
+                 ten++;}
                 else{
                     return false;
                 }
             }
             else{
                 if(five>0 && ten>0){
-                    five--;
                     ten--;
-                    
+                    five--;
                 }
                 else if(five>=3){
                     five-=3;
-                    
                 }
                 else{
                     return false;

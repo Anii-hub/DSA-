4ddec32d33d511e5998fe480e9a296e1a730e624
Anii-hub <chauhananirudh866@gmail.com>
2026-07-08 23:30:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.08%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n=s.size();
         stack<char>st;
-        for(auto num : s){
-            if(num=='(' || num=='[' || num=='{'){
-                st.push(num);
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
             }
-       else{
-            if( st.empty()||num==')' && st.top()!='(' ||num=='}' && st.top()!='{' ||num==']' && st.top()!='['){
+            else{
+                if(st.empty() || s[i]==')' && st.top()!='(' || s[i]=='}' && st.top()!='{' ||s[i]==']' && st.top()!='[' ){
                 return false;
+                }
+                else{
+                    st.pop();
+                }
             }
-            st.pop();
-        }}
+         
+        
+        }
+      
+        
         return st.empty();
     }
 };

7ab2487ba106ba5abc140cfc18b95905cc91437d
Anii-hub <chauhananirudh866@gmail.com>
2026-07-12 00:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (19.62%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result = {-1, -1};
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        result[0] = left;
-        result[1] = right;
-        return result;        
-    }
-
-    int binarySearch(vector<int>& nums, int target, bool isSearchingLeft) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int idx = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+        int n=nums.size();
+        int l=0;
+        int r=n-1;
+        int left=-1;
+        int right=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>target){
+                
+                r=mid-1;
             }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                left=mid;
+                r=mid-1;
+            }
+           
         }
-        
-        return idx;
-    }    
+         l=0;
+         r=n-1;
+         
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>target){
+                
+                r=mid-1;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                right=mid;
+                l=mid+1;
+            }
+           
+        }
+         
+        return {left,right};
+    }
 };

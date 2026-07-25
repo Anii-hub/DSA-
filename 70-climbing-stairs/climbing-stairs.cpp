1c11176ef67a10df9be5110341ddf7ec02d159b6
Anii-hub <chauhananirudh866@gmail.com>
2026-07-25 20:43:24 +0530

    Time: 2 ms (5.13%) | Memory: 8.8 MB (20.36%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int  solve(int n,int i,vector<int>&dp){
+        if(i==n)
+        return 1;
+        if(i>n)
+        return 0;
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int a1=solve(n,i+1,dp);
+        int a2=solve(n,i+2,dp);
+        int ans=a1+a2;
+        dp[i]=ans;
+        return ans;
+
+        
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(n,0,dp);
+    }
+};

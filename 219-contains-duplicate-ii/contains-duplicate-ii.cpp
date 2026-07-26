fd1e1ff01932adb15c1ca01ca933bcd800aaf26a
Anii-hub <chauhananirudh866@gmail.com>
2026-07-26 17:17:30 +0530

    Time: 99 ms (25.20%) | Memory: 124.9 MB (10.37%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+       
+   unordered_map<int,int>mpp;
+    for(int i=0;i<n;i++){
+        int ans=nums[i];
+        if(mpp.find(ans)!=mpp.end() && i-mpp[ans]<=k){
+            return true;
+        }
+        mpp[nums[i]]=i;
+
+    }
+    return false;
+
+
+    }
+
+};

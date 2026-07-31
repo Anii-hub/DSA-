9910d050cd8febee1db9e18ccce7e25d2cd5e19c
Anii-hub <chauhananirudh866@gmail.com>
2026-08-01 01:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (79.35%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+2,0);
+      \
         int prev=0;
         int prev2=0;
         for(int i=n-1;i>=0;i--){
